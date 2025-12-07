40c51feaa093e346ea428069ee7665bd7a44eeb7
Shounak Joshi <shounakjoshi88@gmail.com>
2025-12-07 12:23:46 +0530

    Add largest_of_three.c - Find largest among three

---
+++ largest_of_three.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, largest;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    
+    printf("Largest number: %d\n", largest);
+    
+    return 0;
+}
