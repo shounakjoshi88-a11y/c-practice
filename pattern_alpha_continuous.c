9c259d563df505d5117f4757d7d669386590c587
Shounak Joshi <shounakjoshi88@gmail.com>
2025-12-07 13:02:42 +0530

    Switched to printing alphabets instead of stars, feels like I'm mastering ASCII logic.

---
+++ pattern_alpha_continuous.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n = 4;
+    char ch = 'A';
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            printf("%c ", ch++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ pattern_alpha_palindrome.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n = 5;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-i-1; j++) printf(" ");
+        char ch = 'A';
+        for(int j=0; j<=i; j++) printf("%c", ch++);
+        ch -= 2;
+        for(int j=0; j<i; j++) printf("%c", ch--);
+        printf("\n");
+    }
+    return 0;
+}
